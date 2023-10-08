7c88cd18e0921487eba2a8841bff7c1c68c97bd2
Novian. <humanityisyour@gmail.com>
2023-10-08 21:05:16 +0700

    Update

--- cithara/assets/javascript/search.js
+++ cithara/assets/javascript/search.js
@@ -65,8 +65,101 @@ function searchMin(inputs, items, limit) {
     return sortedItems.slice(0, limit);
 }
 
-function search() {
+function search(inputs, items, filter) {
+    // Split words into array
+    const query = wordcut(inputs);
+    const targetAttribute = ["id", "name", "author", "lyric"];
+    //  In case of no query or item
+    if (query.length === 0 || items.length === 0) {
+        if (query.length === 0) {
+            return items;
+        } else {
+            return [];
+        }
+    }
 
+    if (filter === "all")   {
+        const tolerance = ((-targetAttribute.length * query.length) / 2);
+        // Calculate relevance scores for each item
+        const resultsWithScore = items.map(item => {
+            const attributes = targetAttribute
+                .map((attribute) => item[attribute]) // Get the values of specified attributes
+                .map((value) => (typeof value === 'string' ? value.toLowerCase() : value)); // Convert item values to lowercase
+            
+            const relevanceScore = attributes.reduce((score, attribute) => {
+                // Initialize a penalty value
+                let penalty = 0;
+
+                // Calculate score based on the number of query terms that match
+                const matchCount = query.filter(filter => {
+                    const term = filter.toLowerCase();
+                    if (attribute.toString().toLowerCase().includes(term)) {
+                        return true;
+                    } else {
+                        // If the query term doesn't match, apply a penalty
+                        penalty++;
+                        return false;
+                    }
+                }).length;
+
+                // Subtract the penalty from the score
+                return score + matchCount - penalty;
+            }, 0);
+                
+            return { item, relevanceScore };
+
+        });
+         // Remove item with score below tolerance
+        const filterResults = resultsWithScore.
+        sort((a, b) => b.relevanceScore - a.relevanceScore).
+        filter(result => result.relevanceScore >= tolerance);
+
+        // Extract the sorted items
+        const sortedItems = filterResults.map(result => result.item);
+
+        // Return Array
+        return sortedItems;
+    }
+    else {
+        const tolerance = (query.length / 2);
+        console.log(tolerance);
+
+        const resultsWithScore = items.map(item => {
+            const attributeValue = item[filter]; // Get the attribute value based on the filter string
+            if (typeof attributeValue === 'string') {
+              const loweredAttributeValue = attributeValue.toLowerCase();
+              const relevanceScore = query.reduce((score, queryValue) => {
+                const loweredQueryValue = queryValue.toLowerCase();
+                if (loweredAttributeValue.includes(loweredQueryValue)) {
+                  // If the query term matches, increase the score
+                  return score + 1;
+                } else {
+                  // If the query term doesn't match, do nothing
+                  return score;
+                }
+              }, 0);
+          
+              return { item, relevanceScore };
+            } else {
+              // If the attribute value is not a string, assign a low relevance score
+              return { item, relevanceScore: 0 };
+            }
+          });
+
+          // Remove items with a score below tolerance
+          const filterResults = resultsWithScore.filter(result => result.relevanceScore >= tolerance);
+          
+          // Sort the filtered results by relevance score (descending)
+          filterResults.sort((a, b) => b.relevanceScore - a.relevanceScore);
+          
+          // Extract the sorted items
+          const sortedItems = filterResults.map(result => result.item);
+          
+          // Return Array
+          return sortedItems;
+    }
+
+    
 }
 
 function wordcut(value) {
@@ -77,27 +170,4 @@ function wordcut(value) {
     output = output.filter(i => { return i !== " " });
 
     return output[0] === "" ? [] : output;
-}
-
-function showResults(element, items) {
-    element.textContent = "";
-    element.classList.add("active");
-
-    if(items.length === 0) {
-        element.classList.remove("active");
-        return;
-    }
-    
-    items.forEach(item => {
-        const el = document.createElement("div");
-        el.setAttribute("class", "result");
-        el.setAttribute("onclick", `window.location.href='/cithara/songs/song.html#${ item.id }'`)
-        const img = document.createElement("img");
-        img.setAttribute("src", `/cithara/songs/images/${ item.id }.jpg`);
-        const span = document.createElement("span");
-        span.textContent = item.name;
-        el.appendChild(img);
-        el.appendChild(span);
-        element.appendChild(el);
-    })
 }
