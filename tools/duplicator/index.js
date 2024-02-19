9d1c3e395ad0fa572708d883021c820fb7c55fc9
Novian <95335310+novianStar@users.noreply.github.com>
2024-02-19 18:16:09 +0700

    Add files via upload

--- tools/duplicator/index.js
+++ tools/duplicator/index.js
@@ -3,22 +3,27 @@ const addressElement = document.getElementById('address');
 const postElement = document.getElementById('post');
 const provinceElement = document.getElementById('province');
 const phoneElement = document.getElementById('phone');
+const storeElement = document.getElementById('store');
+
+const date = new Date().toISOString().split('T')[0].replaceAll('-', '/');
 
 const canvas = document.getElementById('canvas');
 
 const ctx = canvas.getContext('2d');
 
 const image = new Image();
-image.src = '1.jpg';
+image.src = '3.jpg';
 image.onload = function () {
     ctx.font = '40px Prompt';
     ctx.fillStyle = '#4c4c4c';
     ctx.drawImage(this, 0, 0, this.width, this.height, 0, 0, canvas.width, canvas.height);
-    ctx.fillText(Name, 55, 408)
-    ctx.fillText(address, 55, 702)
-    ctx.fillText(post, 55, 816);
-    ctx.fillText(province, 55, 931);
-    ctx.fillText(phone, 55, 1110);
+    ctx.fillText(Name, 55, 420)
+    ctx.fillText(address, 55, 714)
+    ctx.fillText(post, 55, 826);
+    ctx.fillText(province, 55, 934);
+    ctx.fillText(phone, 55, 1120);
+    ctx.fillText(store, 55, 1290);
+    ctx.fillText(date, 55, 1469)
 }
 
 let Name = nameElement.value;
@@ -26,15 +31,18 @@ let address = addressElement.value;
 let post = postElement.value;
 let province = provinceElement.value;
 let phone = phoneElement.value;
+let store = storeElement.value;
 
 const drawImage = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvas.width, canvas.height);
-    ctx.fillText(Name, 55, 408)
-    ctx.fillText(address, 55, 702)
-    ctx.fillText(post, 55, 816);
-    ctx.fillText(province, 55, 931);
-    ctx.fillText(phone, 55, 1110);
+    ctx.fillText(Name, 55, 420)
+    ctx.fillText(address, 55, 7000)
+    ctx.fillText(post, 55, 826);
+    ctx.fillText(province, 55, 934);
+    ctx.fillText(phone, 55, 1120);
+    ctx.fillText(store, 55, 1290);
+    ctx.fillText(date, 55, 1469);
 }
 
 nameElement.addEventListener('change', function () {
@@ -51,6 +59,9 @@ provinceElement.addEventListener('change', function () {
 })
 phoneElement.addEventListener('change', function () {
     phone = this.value; drawImage();
+})
+storeElement.addEventListener('change', function () {
+    store = this.value; drawImage();
 })
 
 document.getElementById('submit').addEventListener('click', () => {
