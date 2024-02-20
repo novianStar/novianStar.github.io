12966399216238d2e3f79780e4d302fbff270f71
Novian <95335310+novianStar@users.noreply.github.com>
2024-02-20 23:09:40 +0700

    Add files via upload

--- tools/duplicator/index.js
+++ tools/duplicator/index.js
@@ -5,9 +5,15 @@ const provinceElement = document.getElementById('province');
 const phoneElement = document.getElementById('phone');
 const storeElement = document.getElementById('store');
 
-const date = new Date().toISOString().split('T')[0].replaceAll('-', '/');
+const date = new Date().toLocaleString('en-US', { timeZone: 'Asia/Bangkok'});
+const day = new Date(date).getDate();
+const month = (new Date(date).getMonth() + 1).toString().padStart(2, '0');
+const year = new Date(date).getFullYear();
+const dateFormat = `${year}/${month}/${day}`;
+
 
 const canvas = document.getElementById('canvas');
+const preview = document.getElementById('preview');
 
 const ctx = canvas.getContext('2d');
 
@@ -16,14 +22,7 @@ image.src = '3.jpg';
 image.onload = function () {
     ctx.font = '40px Prompt';
     ctx.fillStyle = '#4c4c4c';
-    ctx.drawImage(this, 0, 0, this.width, this.height, 0, 0, canvas.width, canvas.height);
-    ctx.fillText(Name, 55, 420)
-    ctx.fillText(address, 55, 714)
-    ctx.fillText(post, 55, 826);
-    ctx.fillText(province, 55, 934);
-    ctx.fillText(phone, 55, 1120);
-    ctx.fillText(store, 55, 1290);
-    ctx.fillText(date, 55, 1469)
+    drawImage();
 }
 
 let Name = nameElement.value;
@@ -31,19 +30,7 @@ let address = addressElement.value;
 let post = postElement.value;
 let province = provinceElement.value;
 let phone = phoneElement.value;
-let store = storeElement.value;
-
-const drawImage = () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvas.width, canvas.height);
-    ctx.fillText(Name, 55, 420)
-    ctx.fillText(address, 55, 714)
-    ctx.fillText(post, 55, 826);
-    ctx.fillText(province, 55, 934);
-    ctx.fillText(phone, 55, 1120);
-    ctx.fillText(store, 55, 1290);
-    ctx.fillText(date, 55, 1469);
-}
+let store = document.querySelector(`[name="store"]:checked:not([value="store-misc"])`) ? document.querySelector(`[name="store"]:checked:not([value="store-misc"])`).value : storeElement.value;
 
 nameElement.addEventListener('change', function () {
     Name = this.value; drawImage();
@@ -60,7 +47,7 @@ provinceElement.addEventListener('change', function () {
 phoneElement.addEventListener('change', function () {
     phone = this.value; drawImage();
 })
-storeElement.addEventListener('change', function () {
+storeElement.addEventListener('change', function (event) {
     store = this.value; drawImage();
 })
 
@@ -71,3 +58,41 @@ document.getElementById('submit').addEventListener('click', () => {
     link.setAttribute('href', canvas.toDataURL("image/jpg").replace("image/jpg", "image/octet-stream"));
     link.click();
 })
+
+document.getElementById('reset').addEventListener('click', () => {
+    Name = nameElement.value;
+    address = addressElement.value;
+    post = postElement.value;
+    province = provinceElement.value;
+    phone = phoneElement.value;
+    store = document.querySelector(`[name="store"]:checked:not([value="store-misc"])`) ? document.querySelector(`[name="store"]:checked:not([value="store-misc"])`).value : storeElement.value;
+
+    drawImage();
+})
+
+function storeCheck(event) {
+    const element = event.target;
+    const miscInput = document.getElementById('store');
+    if (element.value === 'store-misc') {
+        miscInput.removeAttribute('disabled');
+        store = miscInput.value;
+    } else {
+        miscInput.setAttribute('disabled', true);
+        store = element.value;
+    }
+
+    drawImage();
+}
+
+function drawImage() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvas.width, canvas.height);
+    ctx.fillText(Name, 55, 420)
+    ctx.fillText(address, 55, 714)
+    ctx.fillText(post, 55, 826);
+    ctx.fillText(province, 55, 934);
+    ctx.fillText(phone, 55, 1120);
+    ctx.fillText(store, 55, 1290);
+    ctx.fillText(dateFormat, 55, 1469);
+    preview.src = canvas.toDataURL('image/jpeg', 1.0);
+}
