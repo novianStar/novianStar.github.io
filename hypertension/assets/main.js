2b94cce27b81121566f1297f14ba01c83175c74a
Novian <95335310+novianStar@users.noreply.github.com>
2024-03-04 22:58:30 +0700

    Delete hypertension directory

--- hypertension/assets/main.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function() {
-    const topbar = document.querySelector("nav div.top-bar");
-
-    if(document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-        topbar?.classList.add("scrolled");
-    } else {
-        topbar?.classList.remove("scrolled");
-    }
-})
